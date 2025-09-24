7e84543989053e1d71b239fe3a7f664a34644bb0
Mauricio-100 <ceoseshell@gmail.com>
2025-09-24 14:06:59 +0100

    Create index.js

---
+++ bin/index.js
@@ -0,0 +1,40 @@
+#!/usr/bin/env node
+import { Command } from 'commander';
+import login from '../commands/login.js';
+import init from '../commands/init.js';
+import publish from '../commands/publish.js';
+import install from '../commands/install.js';
+import whoami from '../commands/whoami.js';
+
+const program = new Command();
+program
+  .name('drn')
+  .description('Dragon CLI – gestion des Dracopacks')
+  .version('0.1.0');
+
+program
+  .command('init')
+  .description('Initialiser un nouveau projet Dragon')
+  .action(init);
+
+program
+  .command('login')
+  .description('Se connecter à la plateforme Dragon')
+  .action(login);
+
+program
+  .command('publish')
+  .description('Publier le paquet courant')
+  .action(publish);
+
+program
+  .command('install <name>')
+  .description('Installer un paquet depuis Dragon')
+  .action(install);
+
+program
+  .command('whoami')
+  .description('Afficher l’utilisateur connecté')
+  .action(whoami);
+
+program.parse();
